f6045a2e1d6422416a843ef8b5800acfcfa09ec6
shubhkhanna <khannashubh04@gmail.com>
2020-10-17 13:51:51 +0530

    added cors header

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,16 @@ const app = express();
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Working");
 });
