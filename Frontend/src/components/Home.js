2b9a58961ba902e269eb90f374fa58728d5f47a5
shubhkhanna <khannashubh04@gmail.com>
2020-10-19 04:55:30 +0530

    Added Home section

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import Roll from "react-reveal/Roll";
+import Modals from "./Modal/Modals";
 
 const Home = () => {
   return (
     <Container>
       <Row className="" style={{ minHeight: "80vh" }}>
-        <Col md={6} className="my-auto text-center order-2 order-md-1 pt-sm-5">
-          <h2>Welcome To Right Companion</h2>
-          <a
-            href="/about"
-            className="btn buttons mt-2 shadow font-weight-bold"
-            style={{ width: "12em" }}
-          >
-            LOGIN
-          </a>
+        <Col md={6} className="my-auto text-center order-2 order-md-1 pt-sm-2">
+          <h4>
+            <Roll top cascade>
+              Welcome To Right Companion
+            </Roll>
+          </h4>
+          <Modals />
         </Col>
         <Col md={6} className="order-1 order-md-2 my-auto">
           <img
