259f1aad88c0958d93e52e9d1a123981c0e4525b
shubhkhanna <khannashubh04@gmail.com>
2020-10-19 04:56:14 +0530

    Added login Modal

---
+++ Frontend/src/components/Modal/Modals.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Modal, ModalBody, Row, Col } from "reactstrap";
+import { FcGoogle, FcLock } from "react-icons/fc";
+import Forms from "../Forms/Forms";
+
+const Modals = () => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  return (
+    <>
+      <button
+        onClick={toggle}
+        className="btn buttons mt-2 shadow font-weight-bold"
+        style={{ width: "10em" }}
+      >
+        LOGIN
+      </button>
+      <Modal
+        isOpen={modal}
+        toggle={toggle}
+        centered
+        style={{ borderRadius: "50px" }}
+      >
+        <ModalBody>
+          <Row className="text-center my-3 ">
+            <Col xs={12}>
+              <FcLock size={60} />
+            </Col>
+            <Col xs={12}>
+              <h3>Sign In</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} className="px-4">
+              <Forms />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} className="text-center">
+              <small className="font-weight-bold">Connect with</small>
+              <a href="/" className="btn" style={{ lineHeight: "0" }}>
+                <FcGoogle size={25} />
+              </a>
+            </Col>
+          </Row>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+};
+
+export default Modals;
