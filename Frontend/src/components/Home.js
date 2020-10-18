08c4c022298abcf9d69fc6829da00749fd58169a
shubhkhanna <khannashubh04@gmail.com>
2020-10-19 01:13:06 +0530

    Css fixes

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -24,29 +24,6 @@ const Home = () => {
         </Col>
       </Row>
     </Container>
-    // <div className="container">
-    //   <div className="row justify-content-center align-items-center">
-    //     <div className="col-md-6 mt-3 mb-3 order-2 order-sm-1">
-    //       <h2>Welcome To Right Companion</h2>
-    //       <div className="text-center mb-3">
-    //         <a
-    //           href="#"
-    //           alt="google"
-    //           className="btn shadow py-2 px-4 mt-3 buttons"
-    //         >
-    //           LOGIN
-    //         </a>
-    //       </div>
-    //     </div>
-    //     <div className="col-md-6 mt-3 order-1 order-sm-2">
-    //       <img
-    //         src={require("../assets/main.svg")}
-    //         alt="booksection"
-    //         className="img-fluid py-5 px-3"
-    //       />
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
