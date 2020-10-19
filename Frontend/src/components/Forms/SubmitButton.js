c89dc04dfbd70d935f50c69633664b91b259fbc5
shubhkhanna <khannashubh04@gmail.com>
2020-10-20 02:25:13 +0530

    added submit button

---
+++ Frontend/src/components/Forms/SubmitButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SubmitButton = (props) => {
+  const { buttonName, disabled } = props;
+  return (
+    <div className="text-center">
+      <button
+        type="submit"
+        className="btn buttons px-3"
+        style={{ width: "10em" }}
+        disabled={disabled}
+      >
+        {buttonName}
+      </button>
+    </div>
+  );
+};
+
+export default SubmitButton;
