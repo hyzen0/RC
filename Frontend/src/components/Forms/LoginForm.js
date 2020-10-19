2ff1a17cb894cb231b2df3aaf1d78eabba44c07a
shubhkhanna <khannashubh04@gmail.com>
2020-10-20 02:21:30 +0530

    Added login form

---
+++ Frontend/src/components/Forms/LoginForm.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import Fields from "./Fields";
+import SubmitButton from "./SubmitButton";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+const onSubmit = (values, onSubmitProps) => {
+  console.log(values);
+  onSubmitProps.setSubmitting(false);
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("Required!"),
+  password: Yup.string()
+    .required("Required!")
+    .min(6, "Password too short - should be of 6 characters."),
+});
+
+const LoginForm = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      validateOnMount
+    >
+      {(formik) => {
+        return (
+          <Form className="form-group">
+            <Fields
+              htmlFor="email"
+              labels="Email*"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="johndoe@example.com"
+            />
+            <Fields
+              htmlFor="password"
+              labels="Password*"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="AbKeyw"
+            />
+            <SubmitButton
+              buttonName="SIGN IN"
+              disabled={!formik.isValid || formik.isSubmitting}
+            />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default LoginForm;
