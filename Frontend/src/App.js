b25e3ac8a903d4161ade94d02dc5ac69533f6a22
shubhkhanna <khannashubh04@gmail.com>
2020-10-19 12:20:31 +0530

    Updated

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 
