9191697d4c747f40f84259ed6ad1647f9c425b8b
shubhkhanna <khannashubh04@gmail.com>
2020-10-20 00:05:12 +0530

    Added school and book section

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,23 +1,19 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import Roll from "react-reveal/Roll";
 import Modals from "../components/Modal/Modals";
-import CardSection from "../components/CardSection";
+import BookSection from "../components/BookSection";
+import SchoolSection from "../components/SchoolSection";
 
 const Home = () => {
   return (
     <>
       <Container>
-        <Row className="" style={{ minHeight: "80vh" }}>
+        <Row style={{ minHeight: "85vh" }}>
           <Col
             md={6}
             className="my-auto text-center order-2 order-md-1 pt-sm-2"
           >
-            <h4>
-              <Roll top cascade>
-                Welcome To Right Companion
-              </Roll>
-            </h4>
+            <h3>Welcome To Right Companion</h3>
             <Modals />
           </Col>
           <Col md={6} className="order-1 order-md-2 my-auto">
@@ -29,7 +25,8 @@ const Home = () => {
           </Col>
         </Row>
       </Container>
-      <CardSection />
+      <BookSection />
+      <SchoolSection />
     </>
   );
 };
