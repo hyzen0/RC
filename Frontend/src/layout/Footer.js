8b69e2326da1641f5cc982e7337f4ba270dc7303
shubhkhanna <khannashubh04@gmail.com>
2020-10-19 14:06:13 +0530

    Changed footer and home page

--- Frontend/src/layout/Footer.js
+++ Frontend/src/layout/Footer.js
@@ -1,12 +1,63 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { MdLocalPhone, MdEmail, MdLocationOn } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <div className="container-fluid" style={{ backgroundColor: "#2F363F" }}>
-      <div className="row py-2">
-        <div className="col text-center text-light">&copy; Copyright 2020</div>
-      </div>
-    </div>
+    <section className="bg-dark">
+      <Container fluid>
+        <Row className="px-4">
+          <Col md={4}>
+            <img
+              src={require("../assets/logo.png")}
+              alt="..."
+              className="img-fluid"
+              width="300"
+              height="300"
+            />
+          </Col>
+          <Col md={4} className="pt-5">
+            <h3 className="font-weight-normal text-white mb-3">Contact</h3>
+            <p className="text-white mb-2">
+              <MdLocalPhone /> +246-542-550-5462
+            </p>
+            <p className="mb-2">
+              <a href="mailto:info@gmail.com" className="text-white">
+                <MdEmail /> info@gmail.com
+              </a>
+            </p>
+            <p className="text-white mb-2">
+              <MdLocationOn /> Street View
+            </p>
+          </Col>
+          <Col md={4} className="pt-5">
+            <h3 className=" font-weight-normal text-white mb-3">Links</h3>
+            <p className="mb-2">
+              <a href="/" className="text-white">
+                Home
+              </a>
+            </p>
+            <p className="mb-2">
+              <a href="/about" className="text-white">
+                About Us
+              </a>
+            </p>
+            <p className="mb-2">
+              <a href="/schools" className="text-white">
+                School List
+              </a>
+            </p>
+          </Col>
+        </Row>
+        <Row class="row">
+          <Col md={12} className="mt-3">
+            <p className="text-center text-white">
+              © Copyright 2020 Right Companion Pvt. Ltd. - All rights reserved.{" "}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -2,28 +2,35 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import Roll from "react-reveal/Roll";
 import Modals from "../components/Modal/Modals";
+import CardSection from "../components/CardSection";
 
 const Home = () => {
   return (
-    <Container>
-      <Row className="" style={{ minHeight: "80vh" }}>
-        <Col md={6} className="my-auto text-center order-2 order-md-1 pt-sm-2">
-          <h4>
-            <Roll top cascade>
-              Welcome To Right Companion
-            </Roll>
-          </h4>
-          <Modals />
-        </Col>
-        <Col md={6} className="order-1 order-md-2 my-auto">
-          <img
-            src={require("../assets/main.svg")}
-            alt="booksection"
-            className="img-fluid"
-          />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row className="" style={{ minHeight: "80vh" }}>
+          <Col
+            md={6}
+            className="my-auto text-center order-2 order-md-1 pt-sm-2"
+          >
+            <h4>
+              <Roll top cascade>
+                Welcome To Right Companion
+              </Roll>
+            </h4>
+            <Modals />
+          </Col>
+          <Col md={6} className="order-1 order-md-2 my-auto">
+            <img
+              src={require("../assets/main.svg")}
+              alt="booksection"
+              className="img-fluid"
+            />
+          </Col>
+        </Row>
+      </Container>
+      <CardSection />
+    </>
   );
 };
 
