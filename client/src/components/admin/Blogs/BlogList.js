c91d2203bb83b87a0d910084c68c4a9dca2a6a35
shubhkhanna <khannashubh04@gmail.com>
2020-12-11 02:12:47 +0530

    Added more content in admin dashboard

--- client/src/components/admin/Blogs/BlogList.js
+++ client/src/components/admin/Blogs/BlogList.js
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import { Row, Col, Spinner, TabPane } from "reactstrap";
 import axios from "axios";
 import BlogTable from "./BlogTable";
+import Paginations from "../../Paginations";
 import replace from "../../../assets/replace.svg";
+import { MdNoteAdd } from "react-icons/md";
 
 const BlogList = props => {
   const { tabId } = props;
 
   const [blogs, setBlogs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [blogsPerPage] = useState(10);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -24,6 +28,14 @@ const BlogList = props => {
       });
   }, []);
 
+  // Get current blogs
+  const indexOfLastBlog = currentPage * blogsPerPage;
+  const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
+  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <TabPane tabId={tabId}>
       {isLoading ? (
@@ -37,17 +49,38 @@ const BlogList = props => {
         </Row>
       ) : (
         <>
+          <Row className="d-flex justify-content-between mt-2 mx-1">
+            <h5 className="text-white">Total Blogs: {blogs.length}</h5>
+            <button type="button" className="btn btn-primary px-4 blogBtn">
+              <MdNoteAdd /> Create New Blog
+            </button>
+          </Row>
+
           {blogs.length === 0 ? (
             <Row className="justify-content-center">
               <Col xs={6} sm={4} md={3} className="pt-5 text-center">
                 <img src={replace} alt="replace" className="img-fluid" />
-                <h4 className="my-3 text-white">
-                  No Blogs! Try Creating New Blogs.
-                </h4>
+                <h4 className="my-3 text-white">No Blogs! Create New Blogs.</h4>
               </Col>
             </Row>
           ) : (
-            <BlogTable blogs={blogs} replace={replace} />
+            <>
+              <Row className="justify-content-center">
+                <BlogTable blogs={currentBlogs} replace={replace} />
+              </Row>
+              <Row className="justify-content-center">
+                <Col>
+                  <h6 className="text-center text-white pt-2">{currentPage}</h6>
+                </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <Paginations
+                  dataPerPage={blogsPerPage}
+                  totalData={blogs.length}
+                  paginate={paginate}
+                />
+              </Row>
+            </>
           )}
         </>
       )}
--- client/src/components/admin/Schools/SchoolList.js
+++ client/src/components/admin/Schools/SchoolList.js
@@ -1,8 +1,29 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import {
+  Row,
+  Col,
+  Spinner,
+  TabPane,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  Button,
+} from "reactstrap";
 import axios from "axios";
+import SchoolTable from "./SchoolTable";
+import Paginations from "../../Paginations";
+import replace from "../../../assets/empty.svg";
+import { MdNoteAdd } from "react-icons/md";
 import { getCookie } from "../../../helpers/auth";
 
-const SchoolList = () => {
+const SchoolList = ({ tabId }) => {
+  const [query, setQuery] = useState("");
+  const [allSchools, setAllSchools] = useState([]);
+  const [schools, setSchools] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [schoolsPerPage] = useState(6);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const token = getCookie("token");
     axios
@@ -11,10 +32,133 @@ const SchoolList = () => {
           Authorization: `Bearer ${token}`,
         },
       })
-      .then(res => console.log(res.data));
+      .then(res => {
+        setIsLoading(false);
+        setAllSchools(res.data);
+      })
+      .catch(err => {
+        setIsLoading(true);
+        console.log(err.response);
+      });
   }, []);
 
-  return <div></div>;
+  const fetchSchools = async () => {
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_API_URL}/api/schools/${
+        query.charAt(0).toUpperCase() + query.slice(1)
+      }`
+    );
+    setIsLoading(true);
+    console.log(data);
+    setTimeout(() => {
+      setQuery("");
+      setIsLoading(false);
+      setSchools(data);
+    }, 500);
+  };
+
+  // Get current schools
+  const indexOfLastSchool = currentPage * schoolsPerPage;
+  const indexOfFirstSchool = indexOfLastSchool - schoolsPerPage;
+  const currentSchools = schools.slice(indexOfFirstSchool, indexOfLastSchool);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  return (
+    <TabPane tabId={tabId}>
+      {isLoading ? (
+        <Row>
+          <Col className="pt-5">
+            <div className="text-center">
+              <Spinner size="lg" color="primary" />
+              <h4 className="text-white">Loading...</h4>
+            </div>
+          </Col>
+        </Row>
+      ) : (
+        <>
+          <Row className="d-flex justify-content-between mt-2 mx-1">
+            <h5 className="text-white">
+              Total Schools in Database: {allSchools.length}
+            </h5>
+            <button type="button" className="btn btn-primary px-4 blogBtn">
+              <MdNoteAdd /> Create New School
+            </button>
+          </Row>
+          {allSchools.length === 0 ? (
+            <Row className="justify-content-center">
+              <Col xs={6} sm={4} md={3} className="pt-5 text-center">
+                <img src={replace} alt="replace" className="img-fluid" />
+                <h4 className="my-3 text-white">
+                  No School! Create New School.
+                </h4>
+              </Col>
+            </Row>
+          ) : (
+            <>
+              <Row className="d-flex justify-content-center mt-2">
+                <Col md={5}>
+                  <InputGroup>
+                    <Input
+                      type="text"
+                      value={query}
+                      onChange={e => setQuery(e.target.value)}
+                      id="cities"
+                      placeholder="Search with city"
+                    />
+                    <InputGroupAddon addonType="append">
+                      {query === "" ? (
+                        <Button
+                          disabled
+                          onClick={fetchSchools}
+                          color="secondary"
+                          className="ml-1 py-1 px-2">
+                          Search
+                        </Button>
+                      ) : (
+                        <Button
+                          onClick={fetchSchools}
+                          color="primary"
+                          className="ml-1 py-1 px-2">
+                          Search
+                        </Button>
+                      )}
+                    </InputGroupAddon>
+                  </InputGroup>
+                </Col>
+              </Row>
+              {schools.length === 0 ? (
+                <Row className="d-flex justify-content-center">
+                  <Col xs={6} sm={4} md={3} className="pt-5 text-center">
+                    <img src={replace} alt="replace" className="img-fluid" />
+                    <h4 className="my-3 text-white">No School! Search</h4>
+                  </Col>
+                </Row>
+              ) : (
+                <>
+                  <Row className="d-flex justify-content-between align-items-center pt-2">
+                    <h6 className="text-white">
+                      Total Schools: {schools.length}
+                    </h6>
+                    <h6 className="text-white">{currentPage}</h6>
+                    <Paginations
+                      dataPerPage={schoolsPerPage}
+                      totalData={schools.length}
+                      paginate={paginate}
+                    />
+                  </Row>
+                  <Row className="d-flex justify-content-center">
+                    <SchoolTable schools={currentSchools} />
+                  </Row>
+                </>
+              )}
+            </>
+          )}
+        </>
+      )}
+    </TabPane>
+  );
 };
 
 export default SchoolList;
